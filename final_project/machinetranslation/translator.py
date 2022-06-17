99cafe815d01c158ea11d50a7441b3c38e855ae4
Gabriel <gabriel51coyote@gmail.com>
2022-06-17 18:42:47 -0500

    Changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,41 +9,48 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-#---------------------------------
-authenticator = IAMAuthenticator('{apikey}')
+# ---------------------------------
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('{url}')
+language_translator.set_service_url(url)
 
-language_translator.set_disable_ssl_verification(True)
-#---------------------------------
+# language_translator.set_disable_ssl_verification(True)
+# ---------------------------------
+
 
 def englishToFrench(englishText):
-    #write the code here
+    """Function return french text from english"""
+    if englishText is None:
+        return None
+
     translation = language_translator.translate(
-    text=englishText,
-    model_id='en-fr').get_result()
+        text=englishText,
+        model_id='en-fr').get_result()
 
-
-    text = json.loads(translation)
+    text = json.loads(json.dumps(translation, indent=2, ensure_ascii=False))
     t_list = text["translations"]
     frenchText = t_list[0]["translation"]
 
     return frenchText
 
-#---------------------------------
-def frenchToEnglish(frenchText):
-    #write the code here
-    translation = language_translator.translate(
-    text=frenchText,
-    model_id='fr-en').get_result()
+# ---------------------------------
 
-    text = json.loads(translation)
+
+def frenchToEnglish(frenchText):
+    """Function returning engglish text from french"""
+    if frenchText is None:
+        return None
+
+    translation = language_translator.translate(
+        text=frenchText,
+        model_id='fr-en').get_result()
+
+    text = json.loads(json.dumps(translation, indent=2, ensure_ascii=False))
     t_list = text["translations"]
     englishText = t_list[0]["translation"]
-
 
     return englishText
